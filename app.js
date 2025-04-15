c08e64264b6ad23384b8690e7dd0efe4f942ca0b
PremMandal12 <178074527+PremMandal12@users.noreply.github.com>
2025-04-15 07:12:46 +0000

    minor changes : v2

--- app.js
+++ app.js
@@ -4,43 +4,14 @@ const app = express()
 
 const port = 3000
 app.use(express.static(path.join(__dirname, '/')))
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, `index.html`))
+  })
+  
+  app.get('/skills', (req, res) => {
+    res.sendFile(path.join(__dirname, `skills.html`))
+  })
 app.listen(port, (error) => {
   console.log(`Server has started successfully!`)
 })
 
-
-// This is one way to display image and text 
-// app.get('/', (req, res) => {
-//   res.send(`<!DOCTYPE html>
-//   <html lang="en">
-//     <head>
-//       <meta charset="UTF-8" />
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//       <title>First Express App</title>
-//       <style>
-//         img {
-//           width: 205px;
-//           border-radius: 125px;
-//         }
-//       </style>
-//     </head>
-//     <body>
-//       <img src="./git-profile-pic.jpg" alt="" />
-//       <h3>
-//         My name is AbdulHadi Bavani & I am currently pursuing B.E. in Computer
-//         Engineering from RCOE
-//       </h3>
-//     </body>
-//   </html>
-// `)
-//})
-
-// Here is a much simpler way 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, `index.html`))
-})
-
-app.get('/skills', (req, res) => {
-  res.sendFile(path.join(__dirname, `skills.html`))
-})
